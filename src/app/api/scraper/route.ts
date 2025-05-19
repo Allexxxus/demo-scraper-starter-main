69020fff0a31b7ba3fa05443e551654fef1b12b1
Allexxxus <crowly83@mail.ru>
2025-05-19 18:50:07 +0300

    web scrapping app test

---
+++ src/app/api/scraper/route.ts
@@ -0,0 +1,29 @@
+import chromium from "@sparticuz/chromium-min";
+import puppeteer from "puppeteer-core";
+
+chromium.setGraphicsMode = false;
+
+export async function POST() {
+  await chromium.font(
+    "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+  );
+
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath(),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://spacejelly.dev");
+  const pageTitle = await page.title();
+  await browser.close();
+
+  return Response.json({
+    // hello: "world",
+    pageTitle
+  });
+}
